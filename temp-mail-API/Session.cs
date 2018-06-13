98c9b9a23a0d06c00edcad72acd8152acefabb95
Hazem Ahmed Amr <hazemahmedamr.zero@gmail.com>
2018-06-13 15:15:14 +0200

    Update Session.cs

--- temp-mail-API/Session.cs
+++ temp-mail-API/Session.cs
@@ -1,174 +1,176 @@
-﻿using System.Net;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using System.Web.Script.Serialization;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Script.Serialization;
+using HtmlAgilityPack;
 
 namespace TempMail
 {
-    class Session
+    public class Session
     {
-
-        private readonly Dictionary<string, Regex> RegexObjects = new Dictionary<string, Regex>()
-        {
-            {"Lines", new Regex("(\n|\r|\r\n)")},
-            {"SpacesBetweenTags", new Regex(@">\s*<")},
-            {"Domains", new Regex("<select name=\"domain\" class=\"form-control\" id=\"domain\">(<option value=\"(?<domain>.*?)\">.*?</option>)+</select>")},
-            {"MailsIds", new Regex("https://temp-mail.org/en/view/(?<id>.*?)\"")},
-            {"Mail", new Regex("class=\"mail opentip\" value=\"(?<email>.*?)\"")}
-        };
-
-        public List<string> AvailableDomains { get { return this.GetAvailableDomains(); } }
-        public List<Mail> Mails { get { return this.GetMails(); } }
-        public string Email { get; set; }
+        private readonly HtmlDocument _document;
 
         private CookieContainer _cookies;
 
+
+        public List<string> AvailableDomains => GetAvailableDomains();
+
+        public Inbox Inbox;
+
+        public string Email { get; set; }
+
+
+
         public Session()
         {
+            _document = new HtmlDocument();
+            _cookies = new CookieContainer();
+
+            Inbox = new Inbox(this);
+
             CreateNewSession();
         }
 
-        /// <summary>
-        /// Gets the Mailbox of the temporary email.
-        /// </summary>
-        public List<Mail> GetMails()
-        {
-            List<Mail> Mails = new List<Mail>();
-
-            var Client = CreateHttpClient();
-            string Response = Client.DownloadString("https://temp-mail.org/en/option/check");
-
-            Response = RegexObjects["Lines"].Replace(Response, "");
-            Response = RegexObjects["SpacesBetweenTags"].Replace(Response, "><");
-
-            List<string> ids = new List<string>();
-
-            var Matches = RegexObjects["MailsIds"].Matches(Response);
-            foreach (Match match in Matches)
-            {
-                var id = match.Groups["id"].Value;
-                if (!ids.Contains(id))
-                {
-                    Mails.Add(Mail.FromID(this, id));
-                    ids.Add(id);
-                }
-            }
-
-            return Mails;
-        }
 
         /// <summary>
-        /// Changes the temporary email to ex: Login@Domain .
+        /// Changes the temporary email to ex: login@domain .
         /// </summary>
-        /// <param name="Login">New temporary email login</param>
-        /// <param name="Domain">New temporary email domain</param>
-        public bool Change(string Login, string Domain)
+        /// <param name="login">New temporary email login</param>
+        /// <param name="domain">New temporary email domain</param>
+        public string Change(string login, string domain)
         {
-            if (!this.AvailableDomains.Contains(Domain))
+            if (!AvailableDomains.Contains(domain))
                 throw new Exception("The domain you entered isn't an available domain");
 
-            var Client = CreateHttpClient();
-
-            Client.Headers.Add("Referer", "https://temp-mail.org/en/option/change");
-            Client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
-
-            var csrf = Client.CookieContainer.GetCookies(new Uri("https://temp-mail.org/"))["csrf"];
-            var data = string.Format("csrf={0}&mail={1}&domain={2}", csrf.Value, Login, "@" + Domain);
-
-            var res = Client.UploadString("https://temp-mail.org/en/option/change", data);
-
-            if (Client.StatusCode == HttpStatusCode.OK)
+            using (var client = CreateHttpClient())
             {
-                this.Email = string.Format("{0}@{1}", Login, Domain);
+                var csrf = GetCsrfCookie(client);
+                var data = BuildUploadString(csrf.Value, login, domain);
 
-                return true;
+                AddUploadHeaders(client);
+
+                client.UploadString("https://temp-mail.org/en/option/change", data);
+
+                if (client.StatusCode != HttpStatusCode.OK)
+                    return null;
+
+                return (Email = $"{login}@{domain}");
             }
-
-            return false;
         }
+
+        private static Cookie GetCsrfCookie(HttpClient client)
+        {
+            return client.CookieContainer.GetCookies(new Uri("https://temp-mail.org/"))["csrf"];
+        }
+
+        private void AddUploadHeaders(HttpClient client)
+        {
+            client.Headers.Set("Referer", "https://temp-mail.org/en/option/change");
+            client.Headers.Set("Content-Type", "application/x-www-form-urlencoded");
+        }
+
+        private string BuildUploadString(string csrf, string login, string domain)
+        {
+            return $"csrf={csrf}&mail={login}&domain={ (domain.Contains("@") ? domain : "@" + domain) }";
+        }
+
 
         /// <summary>
         /// Deletes the temporary email and gets a new one.
         /// </summary>
         public bool Delete()
         {
-            var Client = CreateHttpClient();
-
-            var res = Client.DownloadString("https://temp-mail.org/en/option/delete");
-
-            if (Client.StatusCode == HttpStatusCode.OK)
+            using (var client = CreateHttpClient())
             {
-                var obj = (Dictionary<string, object>)new JavaScriptSerializer().Deserialize<object>(res);
-                this.Email = obj["mail"].ToString();
+                var res = client.DownloadString("https://temp-mail.org/en/option/delete");
+
+                if (client.StatusCode != HttpStatusCode.OK)
+                    return false;
+
+                Email = (new JavaScriptSerializer().Deserialize<object>(res) as Dictionary<string, object>)?["mail"].ToString();
+
+                UpdateEmailCookie();
 
                 return true;
             }
-
-            return false;
         }
+
+        private void UpdateEmailCookie()
+        {
+            _cookies.SetCookies(new Uri("https://temp-mail.org/"), $"mail={Email}");
+        }
+
 
         /// <summary>
         /// Sends a get request to the Url provided using this session cookies and returns the string result.
         /// </summary>
-        /// <param name="Url"></param>
-        public string GET(string Url)
+        /// <param name="url"></param>
+        public string GET(string url)
         {
-            return CreateHttpClient().DownloadString(Url);
+            using (var client = CreateHttpClient())
+            {
+                return client.DownloadString(url);
+            }
         }
 
-        /// <summary>
-        /// Gets the available domains.
-        /// </summary>
         private List<string> GetAvailableDomains()
         {
-            List<string> domains = new List<string>();
+            using (var client = CreateHttpClient())
+            {
+                _document.LoadHtml(client.DownloadString("https://temp-mail.org/en/option/change"));
+            }
 
-            var Client = CreateHttpClient();
+            return ExtractDomains();
+        }
 
-            var res = Client.DownloadString("https://temp-mail.org/en/option/change");
-
-            res = RegexObjects["Lines"].Replace(res, "");
-            res = RegexObjects["SpacesBetweenTags"].Replace(res, "><");
-
-            var Matches = RegexObjects["Domains"].Matches(res);
-            if (Matches.Count > 0)
-                foreach (Capture capture in Matches[0].Groups["domain"].Captures)
-                    domains.Add(capture.Value.Substring(1));
-
-            this._cookies = Client.CookieContainer;
-
-            return domains;
+        private List<string> ExtractDomains()
+        {
+            return _document.GetElementbyId("domain").Descendants("option")
+                .Select(s => s.GetAttributeValue("value", null)).ToList();
         }
 
         private void CreateNewSession()
         {
-            var Client = CreateHttpClient();
+            using (var client = CreateHttpClient())
+            {
+                _document.LoadHtml(LoadMainPage(client));
+            }
 
-            var Response = Client.DownloadString("https://temp-mail.org/en/");
-
-            var Matches = RegexObjects["Mail"].Matches(Response);
-            if (Matches.Count > 0)
-                this.Email = Matches[0].Groups["email"].Value;
-
-            this._cookies = Client.CookieContainer;
+            Email = ExtractEmail();
         }
+
+        private static string LoadMainPage(HttpClient client)
+        {
+            return client.DownloadString("https://temp-mail.org/en/");
+        }
+
+        private string ExtractEmail()
+        {
+            return _document.GetElementbyId("mail").GetAttributeValue("value", null);
+        }
+
 
         /// <summary>
         /// Returns an HttpClient that have this session cookies.
         /// </summary>
         private HttpClient CreateHttpClient()
         {
-            HttpClient Client = new HttpClient();
-            Client.Headers.Add("Accept", "*/*");
-            Client.Headers.Add("Accept-Language", "en-US,en");
-            Client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");
-            Client.Headers.Add("Host", "temp-mail.org");
-            Client.Headers.Add("Origin", "https://temp-mail.org");
-            Client.Headers.Add("X-Requested-With", "XMLHttpRequest");
-            Client.Headers.Add("Upgrade-Insecure-Requests", "1");
-            Client.CookieContainer = this._cookies;
-            return Client;
+            return new HttpClient()
+            {
+                DefaultHeaders = new WebHeaderCollection()
+                {
+                    { "Accept", "*/*" },
+                    { "Accept-Encoding", "gzip, deflate" },
+                    { "Accept-Language", "en-US,en" },
+                    { "User-Agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" },
+                    { "Host", "temp-mail.org" },
+                    { "Origin", "https://temp-mail.org"},
+                    { "X-Requested-With", "XMLHttpRequest"},
+                    { "Upgrade-Insecure-Requests", "1"}
+                },
+                CookieContainer = _cookies
+            };
         }
 
     }
