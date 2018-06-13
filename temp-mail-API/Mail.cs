e3b9e1244e93391b0be148d87358ba3f7e0bfb3f
Hazem Ahmed Amr <hazemahmedamr.zero@gmail.com>
2018-06-13 15:14:47 +0200

    Update Mail.cs

--- temp-mail-API/Mail.cs
+++ temp-mail-API/Mail.cs
@@ -1,54 +1,92 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
+using MimeKit;
 
 namespace TempMail
 {
-    class Mail
+    public class Mail : MimeMessage
     {
-        public string From { get; set; }
-        public string Subject { get; set; }
-        public string Content { get; set; }
-        public string To { get; set; }
-        public string Date { get; set; }
+        public new InternetAddressList From { get; private set; }
+        public new InternetAddressList ResentFrom { get; private set; }
+        public new InternetAddressList ReplyTo { get; private set; }
+        public new InternetAddressList ResentReplyTo { get; private set; }
+        public new InternetAddressList To { get; private set; }
+        public new InternetAddressList ResentTo { get; private set; }
+        public new InternetAddressList Cc { get; private set; }
+        public new InternetAddressList ResentCc { get; private set; }
+        public new InternetAddressList Bcc { get; private set; }
+        public new InternetAddressList ResentBcc { get; private set; }
+        public new string TextBody { get; private set; }
+        public new string HtmlBody { get; private set; }
+        public new HeaderList Headers { get; private set; }
+        public new IEnumerable<MimeEntity> BodyParts { get; private set; }
+        public new IEnumerable<MimePart> Attachments { get; private set; }
+        public string Id { get; set; }
+        public string Link { get; set; }
+        public string StrSender { get; set; }
 
-        /// <summary>
-        /// Return the Mail object of the mail that have the Id provided.
-        /// </summary>
-        /// <param name="session">The session which mail belongs to.</param>
-        /// <param name="Id">The Id of the mail.</param>
-        public static Mail FromID(Session session, string Id)
+
+        public static Mail FromId(Session session, string id)
         {
-            Mail mail = new Mail();
+            var sourceUrl = $"https://temp-mail.org/en/source/{id}";
 
-            string source_url = string.Format("https://temp-mail.org/en/source/{0}", Id);
+            return GetMailFromRaw(session.GET(sourceUrl), id);
+        }
 
-            var source = session.GET(source_url);
-            var result = Regex.Split(source, "\r\n|\n|\r");
+        public static Mail FromLink(Session session, string link)
+        {
+            var id = ExtractId(link);
 
-            foreach (var line in result)
-            {
-                if (line.Length > 0 && line[0] != ' ' && line[0] != '\t' && line.Contains(":"))
-                {
-                    var index = line.IndexOf(':');
+            var sourceUrl = string.Format("https://temp-mail.org/en/source/{0}", id);
 
-                    var name = line.Substring(0, index);
-                    var value = line.Substring(index + 1).Trim();
+            return GetMailFromRaw(session.GET(sourceUrl), id);
+        }
 
-                    if (name == "Subject")
-                        mail.Subject = value;
-                    else if (name == "From")
-                        mail.From = value;
-                    else if (name == "To")
-                        mail.To = value;
-                    else if (name == "Date")
-                        mail.Date = value;
-                }
-            }
-
-            mail.Content = new Regex("--.*\r\nContent-Type: text/plain; charset=UTF-8\r\n\r\n(?<text>.*?)\r\n\r\n--.*", RegexOptions.Singleline).Match(source).Groups["text"].Value.Trim();
+        private static Mail GetMailFromRaw(string raw_mail, string id)
+        {
+            var mail = Parse(raw_mail);
+            mail.Id = id;
 
             return mail;
         }
 
+
+        public static Mail Parse(string raw_mail)
+        {
+            var message = Load(GenerateStreamFromString(raw_mail));
+
+            return ConvertMessageToMail(message);
+        }
+
+        private static MemoryStream GenerateStreamFromString(string value)
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(value ?? ""));
+        }
+
+        private static Mail ConvertMessageToMail(MimeMessage message)
+        {
+            return (Mail)Utilities.CastObjectToAnotherType(message, typeof(Mail));
+        }
+
+
+        public void SaveAttachment(MimePart attachment, string directory = "", string tempFileName = null)
+        {
+            var fileName = attachment.FileName ?? tempFileName;
+
+            using (var stream = File.Create(Path.Combine(directory, fileName)))
+            {
+                attachment.Content.DecodeTo(stream);
+            }
+        }
+
+
+        public static string ExtractId(string link)
+        {
+            return Regex.Match(link, @"https://temp-mail.org/en/.*?/(?<id>.*)").Groups["id"].Value;
+        }
+
     }
+
 }
